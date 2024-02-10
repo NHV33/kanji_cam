f5547631b4edff84a443d35dd202b52ae14bec6a
Taka Kamio <takahiro.1203.k@gmail.com>
2024-02-10 13:26:58 +0900

    working seed

--- app/models/kanji.rb
+++ app/models/kanji.rb
@@ -1,6 +1,5 @@
 class Kanji < ApplicationRecord
-  validates :character, presence: true # Validate length
-  validates :stroke_count, presence: true
+  validates :strokes, presence: true
   validates :character, presence: true, length: { is: 1 }
 
   acts_as_taggable_on :meanings, :on_readings, :kun_readings
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,25 +9,53 @@
 #   end
 require "csv"
 
-csv_file_path = Rails.root.join("lib", "seeds", "joyo.csv")
+# User seeds
 
-CSV.foreach(csv_file_path, headers: true).with_index(1) do |row, index|
-  break if index > 10
-
-  kanji = Kanji.new(
-    character: row["kanji"],
-    radical: row["radical"],
-    jlpt_level: row["jlpt"].nil? || row["jlpt"].empty? ? 0 : row["jlpt"].to_i,
-    grade: row["grade"] == "S" ? 7 : row["grade"].to_i,
-    stroke_count: row["strokes"].to_i,
-    frequency: row["frequency"].nil? || row["frequency"].empty? ? 9999999 : row["frequency"].to_i,
+if User.all.empty?
+  user = User.new(
+    email: "test@me.com",
+    password: "Kanji-ga-daisuki!",
   )
-  kanji.save!
+  user.save!
+end
 
-  kanji.meaning_list = row["meanings"].split("|") unless row["meanings"].nil?
-  kanji.on_reading_list = row["on"].split("|") unless row["on"].nil?
-  kanji.kun_reading_list = row["kun"].split("|") unless row["kun"].nil?
+# Kanji seeds
+if Kanji.all.empty?
+  csv_file_path = Rails.root.join("lib", "seeds", "joyo.csv")
 
-  p kanji
-  kanji.save!
+  CSV.foreach(csv_file_path, headers: true).with_index(1) do |row, index|
+    break if index > 100
+
+    kanji = Kanji.new(
+      character: row["kanji"],
+      radical: row["radical"],
+      jlpt: row["jlpt"].nil? || row["jlpt"].empty? ? 0 : row["jlpt"].to_i,
+      grade: row["grade"] == "S" ? 7 : row["grade"].to_i,
+      strokes: row["strokes"].to_i,
+      frequency: row["frequency"].nil? || row["frequency"].empty? ? 9_999_999 : row["frequency"].to_i,
+    )
+    kanji.meaning_list = row["meanings"].split("|") unless row["meanings"].nil?
+    kanji.on_reading_list = row["on"].split("|") unless row["on"].nil?
+    kanji.kun_reading_list = row["kun"].split("|") unless row["kun"].nil?
+    kanji.save!
+  end
+end
+
+# Card seeds
+if Card.all.empty?
+  Kanji.all.each do |kanji|
+    card = Card.new(
+      user_id: User.find_by(email: "test@me.com").id,
+      kanji_id: kanji.id,
+      learned: false,
+      practice_count: (1..5).to_a.sample,
+      prev_practice_at: nil,
+      next_practice_at: nil,
+      latitude: rand(35.65...35.69),
+      longitude: rand(139.70...139.75),
+      favorite: false,
+      comment: nil,
+    )
+    card.save!
+  end
 end
