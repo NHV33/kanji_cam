f1961286ae005dd0b88813305e1fbc705ed4c0f0
Taka Kamio <takahiro.1203.k@gmail.com>
2024-02-10 12:09:57 +0900

    modify seed

--- app/models/kanji.rb
+++ app/models/kanji.rb
@@ -3,7 +3,5 @@ class Kanji < ApplicationRecord
   validates :stroke_count, presence: true
   validates :character, presence: true, length: { is: 1 }
 
-  acts_as_taggable_on :meanings
-  acts_as_taggable_on :on_readings
-  acts_as_taggable_on :kun_readings
+  acts_as_taggable_on :meanings, :on_readings, :kun_readings
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-require 'csv'
+require "csv"
 
-csv_file_path = Rails.root.join('lib', 'seeds', 'joyo.csv')
+csv_file_path = Rails.root.join("lib", "seeds", "joyo.csv")
 
 CSV.foreach(csv_file_path, headers: true).with_index(1) do |row, index|
   break if index > 10
 
   kanji = Kanji.new(
-    character: row['kanji'],
-    radical: row['radical'],
-    jlpt_level: row['jlpt'].nil? || row['jlpt'].empty? ? 0 : row['jlpt'].to_i,
-    grade: row['grade'] == 'S' ? 7 : row['grade'].to_i,
-    stroke_count: row['strokes'].to_i,
-    frequency: row['frequency'].nil? || row['frequency'].empty? ? 9999999 : row['frequency'].to_i
+    character: row["kanji"],
+    radical: row["radical"],
+    jlpt_level: row["jlpt"].nil? || row["jlpt"].empty? ? 0 : row["jlpt"].to_i,
+    grade: row["grade"] == "S" ? 7 : row["grade"].to_i,
+    stroke_count: row["strokes"].to_i,
+    frequency: row["frequency"].nil? || row["frequency"].empty? ? 9999999 : row["frequency"].to_i,
   )
+  kanji.save!
 
-  kanji.meaning_list = row['meanings']
-  kanji.on_reading_list = row['on'].split('|') unless row['on'].nil?
-  kanji.kun_reading_list = row['kun'].split('|') unless row['kun'].nil?
+  kanji.meaning_list = row["meanings"].split("|") unless row["meanings"].nil?
+  kanji.on_reading_list = row["on"].split("|") unless row["on"].nil?
+  kanji.kun_reading_list = row["kun"].split("|") unless row["kun"].nil?
 
-  if kanji.save
-    puts "Kanji #{kanji.character} created successfully."
-  else
-    puts "Error creating Kanji #{kanji.character}: #{kanji.errors.full_messages.join(", ")}"
-  end
+  p kanji
+  kanji.save!
 end
