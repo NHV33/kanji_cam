25a3408711de23f8d51ba1b6d4778c2656ff17db
Funayama Miho <funamiho1011@gmail.com>
2024-02-11 18:36:34 +0900

    addressed suggested solution and style change to show page

--- app/models/kanji.rb
+++ app/models/kanji.rb
@@ -3,7 +3,7 @@ class Kanji < ApplicationRecord
   validates :character, presence: true, length: { is: 1 } #validate length
   validates :grade, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
   validates :strokes, presence: true, numericality: { greater_than: 0 }
-  validates :jlpt, inclusion: { in: 1..5 }, allow_nil: true
+  validates :jlpt, inclusion: { in: 0..5 }
   validates :frequency, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 
   acts_as_taggable_on :meanings, :on_readings, :kun_readings
