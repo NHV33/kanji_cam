7b793cc266e461492ace80b96c195c9af3d459ca
Noel Vincent <noelvincent@me.com>
2024-02-15 20:10:19 +0900

    added template for card crud to cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,4 +6,37 @@ class CardsController < ApplicationController
   def show
     @card = Card.find(params[:id])
   end
+
+  # def create
+  #   @card = current_user.cards.build(card_params)
+  #   if @card.save
+  #    redirect_to @card
+  #   else
+  #     render :new, status: 422
+  #   end
+  # end
+
+  # def new
+  #   @card = Card.new
+  # end
+
+  # def update
+  #   @card = Card.find(params[:id])
+  #   if @card.update(card_params)
+  #    redirect_to @card, notice: "Information updated!"
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+  # def edit
+  #   @card = Card.find(params[:id])
+  # end
+
+  # private
+
+  # def card_params
+  #   params.require(:card).permit(:user, )
+  # end
+
 end
