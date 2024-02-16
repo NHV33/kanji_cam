ade4a5be87f2200639fd8244dda9b9ca1c6af918
Noel Vincent <noelvincent@me.com>
2024-02-16 20:58:22 +0900

    camera script running properly with minor reload-related bug

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,18 +7,36 @@ class CardsController < ApplicationController
     @card = Card.find(params[:id])
   end
 
-  # def create
-  #   @card = current_user.cards.build(card_params)
-  #   if @card.save
-  #    redirect_to @card
-  #   else
-  #     render :new, status: 422
-  #   end
-  # end
+  def create
+    # @card = current_user.cards.build(card_params)
+    @card = Card.new
+    @card.kanji = params(:kanji_data)
+    if @card.save
+     redirect_to @card
+    else
+      render :new, status: 422
+    end
+  end
 
-  # def new
-  #   @card = Card.new
-  # end
+  def new
+    @card = Card.new
+  end
+
+  def capture
+    # p params[:kanji_data]
+  end
+
+  def new_capture
+    new_kanji = params[:kanji_data]
+    @card = Card.new
+    @card.user_id = current_user.id
+    # @card.kanji_id = 1
+    @card.kanji_id = Kanji.find_by(character: "員").id
+
+    if @card.save!
+      redirect_to @card
+    end
+  end
 
   # def update
   #   @card = Card.find(params[:id])
@@ -36,7 +54,7 @@ class CardsController < ApplicationController
   # private
 
   # def card_params
-  #   params.require(:card).permit(:user, )
+  #   params.require(:card).permit(:user)
   # end
 
 end
