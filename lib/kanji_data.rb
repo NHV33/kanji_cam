22235e8b06919217de0def2096ad0e4e29a61421
Noel Vincent <noelvincent@me.com>
2024-02-20 17:53:09 +0900

    KanjiData class - cleaned up unused code & added note

--- lib/kanji_data.rb
+++ lib/kanji_data.rb
@@ -1,6 +1,6 @@
 require "csv"
 
-$csv_file_path = Rails.root.join("lib", "seeds", "joyo.csv")
+$kanji_csv_path = Rails.root.join("lib", "seeds", "joyo.csv")
 
 class KanjiData
   attr_reader :exists, :row, :character, :radical, :jlpt, :grade, :strokes, :frequency, :meaning_list, :on_reading_list, :kun_reading_list
@@ -25,18 +25,6 @@ class KanjiData
       @kun_reading_list = row["kun"].nil? || row["kun"].empty? ? [] : row["kun"].split("|")
     end
   end
-    # def initialize
-  #   @row = nil
-  #   @character = nil
-  #   @radical = nil
-  #   @jlpt = nil
-  #   @grade = nil
-  #   @strokes = nil
-  #   @frequency = nil
-  #   @meaning_list = nil
-  #   @on_reading_list = nil
-  #   @kun_reading_list = nil
-  # end
 
   def exists?
     return @exists
@@ -44,27 +32,14 @@ class KanjiData
 
   private
 
-  # def find(search_term)
   def find(search_term)
     search_col = "kanji"
     search_col = "index" if search_term.is_a?(Integer)
     search_term = search_term.to_s
 
-    CSV.foreach($csv_file_path, headers: true).with_index(1) do |row, index|
+    CSV.foreach($kanji_csv_path, headers: true).with_index(1) do |row, index|
       if row[search_col] == search_term
         return row
-        # return {
-        #   id: row["index"],
-        #   character: row["kanji"],
-        #   radical: row["radical"],
-        #   jlpt: row["jlpt"].nil? || row["jlpt"].empty? ? 0 : row["jlpt"].to_i,
-        #   grade: row["grade"] == "S" ? 7 : row["grade"].to_i,
-        #   strokes: row["strokes"].to_i,
-        #   frequency: row["frequency"].nil? || row["frequency"].empty? ? 9_999_999 : row["frequency"].to_i,
-        #   meaning_list: row["meanings"].nil? || row["meanings"].empty? ? [] : row["meanings"].split("|"),
-        #   on_reading_list: row["on"].nil? || row["on"].empty? ? [] : row["on"].split("|"),
-        #   kun_reading_list: row["kun"].nil? || row["kun"].empty? ? [] : row["kun"].split("|"),
-        # }
       end
     end
 
@@ -72,28 +47,18 @@ class KanjiData
   end
 end
 
+# If implemented as a class method: <== chose NOT to do this
+
 # example_kanji = KanjiData.find(444)
 
 # p example_kanji[:character]
 
+# ---------------------------------------------
+
+# If implemented as an instance method: <== chose to do this
+# With instance methods, we can use dot notation, which is more
+# consistent with the rest of the ActiveRecord code (e.g., "Card.all.first", etc.)
+
 # example_kanji = KanjiData.new(2200)
 
 # p example_kanji.exists?
-
-# row = example_kanji.row
-# kun = example_kanji.kun_reading_list
-# on = example_kanji.on_reading_list
-# puts "found: #{on}, #{kun} #{row["kanji"]}"
-
-
-# return {
-#   character: row["kanji"],
-#   radical: row["radical"],
-#   jlpt: row["jlpt"].nil? || row["jlpt"].empty? ? 0 : row["jlpt"].to_i,
-#   grade: row["grade"] == "S" ? 7 : row["grade"].to_i,
-#   strokes: row["strokes"].to_i,
-#   frequency: row["frequency"].nil? || row["frequency"].empty? ? 9_999_999 : row["frequency"].to_i,
-#   meaning_list: row["meanings"].split("|"),
-#   on_reading_list: row["on"].split("|"),
-#   kun_reading_list: row["kun"].split("|")
-# }
