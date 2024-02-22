90c3862c6976327d6c8385420c538ea8084c9fa4
Miho Funayama <funamiho1011@gmail.com>
2024-02-22 20:51:07 +0900

    confetti controller working

--- app/javascript/controllers/confetti_controller.js
+++ app/javascript/controllers/confetti_controller.js
@@ -1,46 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
+import confetti from "https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.2/+esm";
 
 // connect to data-controller "confetti"
 export default class extends Controller {
 
   connect() {
     console.log("confetti connected");
+    this.confirm();
   }
 
-    confirm() {
+  confirm() {
     // confetti
-    // var duration = 8 * 1000;
-
-    var startTime = Date.now();
-
-    var formSubmitted = false;
-
-  function frame() {
     // launch a few confetti from the left edge
     confetti({
-      particleCount: 7,
+      particleCount: 100,
       angle: 60,
       spread: 55,
       origin: { x: 0 }
     });
     // and launch a few from the right edge
     confetti({
-      particleCount: 7,
+      particleCount: 100,
       angle: 120,
       spread: 55,
       origin: { x: 1 }
     });
-
-    // // submit the form after confetti duration has passed
-    // if (Date.now() - startTime < duration) {
-    //   requestAnimationFrame(frame);
-    // } else {
-    //   if (!formSubmitted) {
-    //     document.getElementById('kanji-form').submit();
-    //     formSubmitted = true;
-    //   }
-    // }
-  };
-  frame();
   }
 };
