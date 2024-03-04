ab0f467fc6bc5807b6925c2d11d4d19e65581dc5
Miho Funayama <funamiho1011@gmail.com>
2024-03-04 19:31:48 +0900

    added zoom in for user marker click

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,13 +13,11 @@ export default class extends Controller {
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v11",
-      // center: [0, 0],
-      zoom: 0
+      center: [139.70817, 35.63395], //Le Wagon Tokyo!
+      zoom: 1
     });
 
     this.map.on('load', () => {
-      // this.map.setCenter([0, 0]);
-      // this.map.setZoom(0);
     // get user's location and display the pin, add other markers as well
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -53,19 +51,51 @@ export default class extends Controller {
     const userMarker = document.createElement('div');
     userMarker.className = 'user-marker';
 
+    const userSpeechBubble = document.createElement("div");
+    userSpeechBubble.className = "user-speech-bubble";
+    userMarker.appendChild(userSpeechBubble);
+
+    const speechBubbleText = document.createElement("p");
+    speechBubbleText.className = "speech-bubble-text";
+    speechBubbleText.textContent = "You are here!"
+    userSpeechBubble.appendChild(speechBubbleText);
+
     new mapboxgl.Marker(userMarker)
           .setLngLat([coords.longitude, coords.latitude])
           .addTo(this.map);
           console.log(userMarker, "user marker created");
 
         // adjust map showing range to user's location
-        // comment out as this animation can be annoying
+        // comment out if this animation can be annoying
         this.map.flyTo({
             center: [coords.longitude, coords.latitude], // set user's location to the center
             zoom: 12,
             essential: true,
-            duration: 1000
+            duration: 2000
         });
+
+        // userMarker.getElement().addEventListener("click", () => {
+        //     this.map.flyTo({
+        //       center: [marker.lng, marker.lat], // set marker in the center
+        //       zoom: 12, // zoom level
+        //       essential: true, // animation: true
+        //       duration: 1000,
+        //     });
+        //   })
+
+        userMarker.addEventListener('click', () => {
+        const currentZoom = this.map.getZoom();
+        let newZoomLevel = currentZoom >= 12 ? currentZoom + 2 : 12;
+        // Limit zoom up to Mapbox maximum zoom level
+        newZoomLevel = Math.min(newZoomLevel, this.map.getMaxZoom());
+
+        this.map.flyTo({
+          center: [coords.longitude, coords.latitude],
+          zoom: newZoomLevel,
+          essential: true,
+          duration: 1000
+              });
+    })
 
       this.map.addControl(
       new mapboxgl.GeolocateControl({
@@ -99,6 +129,53 @@ export default class extends Controller {
           .setLngLat([marker.lng, marker.lat])
           .addTo(this.map);
 
+          markerIcon.addEventListener("click", () => {
+            const activeMarkers =
+              document.getElementsByClassName("pinpoint-active");
+            if (popup) {
+              popup.remove();
+              for (let i = 0; i < activeMarkers.length; i++) {
+                activeMarkers[i].classList.remove("pinpoint-active");
+                // console.log(document.getElementsByClassName("pinpoint-active"));
+              }
+            }
+            // markerIcon.classList.add("pinpoint-active");
+            markerPinPoint.classList.add("pinpoint-active");
+            // markerPinPoint.style.zIndex = 9999;
+            const meaningText = `<p class="m-0">Meaning: ${marker.meaning}</p>`;
+            const linkText = `<a href="/kanjis/${marker.kanji_id}">Check details</a>`;
+
+            const onText = `<p class="m-0">on reaning: ${marker.on_reading}</p>`;
+            const kunText = `<p class="m-0">kun reaning: ${marker.kun_reading}</p>`;
+            const formattedDate = new Date(
+              marker.captured_date
+            ).toLocaleDateString("en-us", {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            });
+            const dateText = `<p class="m-0">Added on: ${formattedDate}</p>`;
+            console.log(formattedDate, "formatted date");
+
+            popup = new mapboxgl.Popup({
+              closeButton: false,
+              closeOnClick: false,
+            })
+              .setLngLat([marker.lng, marker.lat])
+              .setHTML(meaningText + onText + kunText + dateText + linkText)
+              .setOffset([0, -50])
+              .addTo(this.map);
+
+            // When marker is clicked, zoom in as needed
+            console.log("fly to marker");
+            this.map.flyTo({
+              center: [marker.lng, marker.lat], // set marker in the center
+              zoom: 12, // zoom level
+              essential: true, // animation: true
+              duration: 1000,
+            });
+          });
+
           newMarker.getElement().addEventListener('click', () => {
             const currentZoom = this.map.getZoom();
             let newZoomLevel = currentZoom >= 12 ? currentZoom + 2 : 12;
@@ -111,68 +188,8 @@ export default class extends Controller {
           essential: true,
           duration: 1000
               });
-
-        markerIcon.addEventListener("click", () => {
-          const activeMarkers =
-            document.getElementsByClassName("pinpoint-active");
-          if (popup) {
-            popup.remove();
-            for (let i = 0; i < activeMarkers.length; i++) {
-              activeMarkers[i].classList.remove("pinpoint-active");
-              // console.log(document.getElementsByClassName("pinpoint-active"));
-            }
-          }
-          // markerIcon.classList.add("pinpoint-active");
-          markerPinPoint.classList.add("pinpoint-active");
-          // markerPinPoint.style.zIndex = 9999;
-          const meaningText = `<p class="m-0">Meaning: ${marker.meaning}</p>`;
-          const linkText = `<a href="/kanjis/${marker.kanji_id}">Check details</a>`;
-
-          const onText = `<p class="m-0">on reaning: ${marker.on_reading}</p>`;
-          const kunText = `<p class="m-0">kun reaning: ${marker.kun_reading}</p>`;
-          const formattedDate = new Date(
-            marker.captured_date
-          ).toLocaleDateString("en-us", {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-          });
-          const dateText = `<p class="m-0">Added on: ${formattedDate}</p>`;
-          console.log(formattedDate, "formatted date");
-
-          popup = new mapboxgl.Popup({
-            closeButton: false,
-            closeOnClick: false,
-          })
-            .setLngLat([marker.lng, marker.lat])
-            .setHTML(meaningText + onText + kunText + linkText + dateText)
-            .setOffset([0, -50])
-            .addTo(this.map);
-
-          // When marker is clicked, zoom in as needed
-          console.log("fly to marker");
-          this.map.flyTo({
-            center: [marker.lng, marker.lat], // set marker in the center
-            zoom: 12, // zoom level
-            essential: true, // animation: true
-            duration: 1000,
-          });
-        });
       })
     };
   })
 }
-
-// #fitMapToMarkers(userCoords = null) {
-//   const bounds = new mapboxgl.LngLatBounds();
-//   if (userCoords) {
-//     bounds.extend([userCoords.longitude, userCoords.latitude]);
-//   }
-//   this.markersValue.forEach((marker) => {
-//     bounds.extend([marker.lng, marker.lat]);
-//   });
-//   if (!bounds.isEmpty()) {
-//     this.map.fitBounds(bounds, { padding: 50, duration: 0 });
-//   }
-// }
 }
